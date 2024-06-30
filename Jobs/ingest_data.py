b0742084b8af392736d1469fe030bb1cb575dd72
HsiuChuanHsu <hchsu2106@gmail.com>
2024-07-01 07:51:09 +0800

    spark streaming test

---
+++ Jobs/ingest_data.py
@@ -0,0 +1,73 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+import pyspark.sql.functions as fn
+
+
+KAFKA_TOPIC_NAME_CONS = "users_created"
+KAFKA_BOOTSTRAP_SERVERS_CONS = 'localhost:9092' #'broker:29092' 
+
+def create_selection_df_from_kafka(spark_df):
+    schema = StructType([
+        StructField("id", StringType(), False),
+        StructField("first_name", StringType(), False),
+        StructField("last_name", StringType(), False),
+        StructField("gender", StringType(), False),
+        StructField("address", StringType(), False),
+        StructField("post_code", StringType(), False),
+        StructField("email", StringType(), False),
+        StructField("username", StringType(), False),
+        StructField("registered_date", StringType(), False),
+        StructField("phone", StringType(), False),
+        StructField("picture", StringType(), False)
+    ])
+
+    sel = spark_df.selectExpr("CAST(value AS STRING)") \
+        .select(from_json(col('value'), schema).alias('data')).select("data.*")
+    print(sel)
+
+    return sel
+
+if __name__ == "__main__":
+    mongoURL = 'mongodb://mongadmin:mongadmin@localhost:27017/UserData.User'
+
+    spark = SparkSession.builder\
+        .appName("Real-Time Streaming Data Pipeline")\
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.2")\
+        .getOrCreate()
+    
+        #"org.mongodb.spark:mongo-spark-connector_2.11:2.41")\
+        # .config("spark.mongodb.read.connection.uri", mongoURL) \
+        # .config("spark.mongodb.write.connection.uri", mongoURL) \
+        # "org.mongodb.spark:mongo-spark-connector_2.12:10.3.0") \
+        # .master("local[*]")\
+    
+    liveData = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS_CONS) \
+        .option("subscribe", KAFKA_TOPIC_NAME_CONS) \
+        .option("startingOffsets", "earliest") \
+        .load()
+    # liveData = liveData.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+    liveData = create_selection_df_from_kafka(liveData)
+
+
+    
+    # Write the streaming data to the console
+    query = liveData.writeStream \
+        .outputMode("append") \
+        .format("console") \
+        .option("truncate", "false") \
+        .start()
+
+    # def write_mongo_row(df, epoch_id):
+    #     df.write.format("mongo")\
+    #         .mode("append")\
+    #         .option("uri",mongoURL)\
+    #         .save()
+    #     pass
+
+    # query = liveData.writeStream.foreachBatch(write_mongo_row).start()
+    
+    query.awaitTermination()
