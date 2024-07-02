6860d4a4d393b52e876d26f748a24ff1051f9540
HsiuChuanHsu <hchsu2106@gmail.com>
2024-07-02 22:06:16 +0800

    test

--- Jobs/ingest_data.py
+++ Jobs/ingest_data.py
@@ -29,7 +29,7 @@ def create_selection_df_from_kafka(spark_df):
     return sel
 
 if __name__ == "__main__":
-    mongoURL = 'mongodb://mongadmin:mongadmin@localhost:27017/UserData.User'
+    mongoURL = 'mongodb://mongadmin:mongadmin@localhost:27017/UserData.User' #local.startup_log
 
     spark = SparkSession.builder\
         .appName("Real-Time Streaming Data Pipeline")\
@@ -37,15 +37,7 @@ if __name__ == "__main__":
                 "org.mongodb.spark:mongo-spark-connector_2.13:10.3.0")\
         .config("spark.mongodb.read.connection.uri", mongoURL) \
         .config("spark.mongodb.write.connection.uri", mongoURL) \
-        .getOrCreate()
-        
-        
-
-        #"org.mongodb.spark:mongo-spark-connector_2.11:2.41")\
-        # .config("spark.mongodb.read.connection.uri", mongoURL) \
-        # .config("spark.mongodb.write.connection.uri", mongoURL) \
-        # "org.mongodb.spark:mongo-spark-connector_2.12:10.3.0") \
-        # .master("local[*]")\
+        .getOrCreate()        
     
     liveData = spark \
         .readStream \
@@ -54,25 +46,37 @@ if __name__ == "__main__":
         .option("subscribe", KAFKA_TOPIC_NAME_CONS) \
         .option("startingOffsets", "earliest") \
         .load()
-    # liveData = liveData.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-    liveData = create_selection_df_from_kafka(liveData)
-
 
     
     # Write the streaming data to the console
+    liveData = create_selection_df_from_kafka(liveData)
     # query = liveData.writeStream \
     #     .outputMode("append") \
     #     .format("console") \
     #     .option("truncate", "false") \
     #     .start()
+    # query.awaitTermination()
 
     def write_mongo_row(df, epoch_id):
-        df.write.format("com.mongodb.spark.sql.DefaultSource")\
-            .mode("append")\
-            .option("uri", mongoURL) \
-            .save()
-        pass
+        
+        try:
+            df.write.format("mongo")\
+                .mode("append")\
+                .option("uri", 'mongodb://mongadmin:mongadmin@localhost:27017')\
+                .option("database", "UserData") \
+                .option("collection", "User") \
+                .option("convertJson", "any") \
+                .save()
+        except Exception as e:
+            print(f"Error writing to MongoDB: {e}")
+        
+        # df.show()
+        # com.mongodb.spark.sql.DefaultSource
+        # .option("ordered", "false")\
+        # .option("replaceDocument", "false")\
 
-    query = liveData.writeStream.foreachBatch(write_mongo_row).start()
-    
+    query = liveData.writeStream\
+        .foreachBatch(write_mongo_row)\
+        .start()
+    #.trigger(processingTime='10 seconds')\
     query.awaitTermination()
